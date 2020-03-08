9bec7b459ec9c3a5106772d1e230d3baf46840bf
Diego Domínguez <d.dominguez.glez1111@gmail.com>
2020-03-08 13:16:11 +0100

    Initial Commit

---
+++ UITableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewExample
+//
+//  Created by Diego Dominguez on 08/03/2020.
+//  Copyright © 2020 Diego Dominguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
